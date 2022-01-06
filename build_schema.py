e79a5b0bec599da1e51a937d7e4a33cfec059d65
Tom Coufal <7453394+tumido@users.noreply.github.com>
2022-01-06 14:50:17 +0100

    feat: Build a skip kinds file (#5)
    * feat: Add file for skipping kinds in kubeval and way to generate it
    
    Signed-off-by: Michal Drla <mdrla@redhat.com>
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    * feat: Build a skip kinds list
    
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>
    
    Co-authored-by: Michal Drla <mdrla@redhat.com>

--- build_schema.py
+++ build_schema.py
@@ -16,10 +16,12 @@ import ssl
 import yaml
 import json
 import subprocess
+from dataclasses import dataclass
 
 existing_definitions_file = None
 
 OPENSHIFT_API_SPEC_FILE = "openshift-api-spec.json"
+SKIP_KINDS_FILE = "skip_kinds.txt"
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -91,29 +93,82 @@ def loadYAML(location):
 
 openapi_endpoint = "{}/openapi/v2".format(args.url)
 
-print("Downloading and parsing API schema from: {}\n".format(openapi_endpoint))
+print("Downloading and parsing API schema from: {}".format(openapi_endpoint))
 
 openapi_data = loadYAML(openapi_endpoint)
 
 definitions = openapi_data["definitions"]
-delete_list = []
 spec_file = os.path.join(script_dir, OPENSHIFT_API_SPEC_FILE)
 
 
+@dataclass(frozen=True)
+class Resource:
+    group: str
+    kind: str
+    version: str
+    has_schema: bool
+    type_name: str
+
+
+resources: set[Resource] = set()
+
+# Parse all definitions into a flat set of resources
+for type_name in definitions:
+    type_def = definitions[type_name]
+    if "x-kubernetes-group-version-kind" in type_def:
+        for kube_ext in type_def["x-kubernetes-group-version-kind"]:
+            resources.add(
+                Resource(
+                    kube_ext["group"],
+                    kube_ext["kind"],
+                    kube_ext["version"],
+                    "properties" in type_def,
+                    type_name,
+                )
+            )
+
+# Read previously saved SKIP_KIND_FILE
+skip_kinds_file = os.path.join(script_dir, SKIP_KINDS_FILE)
+try:
+    with open(skip_kinds_file, "r+") as skf:
+        skip_kinds = set(skf.readlines()[0].split(","))
+except (IOError, IndexError):
+    skip_kinds: set[str] = set()
+
+# Build a list of resources which don't have a proper schema, which means they:
+# 1. would fail to generate schema in schema store, hence skip them there (delete_list)
+# 2. their Kind may be skipped if there's no other version of the resource with proper schema
+
+delete_list: set[str] = set()
+for i in resources:
+    if not i.has_schema:
+        delete_list.add(i.type_name)
+        if next(
+            filter(
+                lambda r: r.group == i.group and r.kind == i.kind and r.has_schema,
+                resources,
+            ),
+            False,
+        ):
+            # different api version for given kind has schema
+            continue
+        skip_kinds.add(i.kind)
+
+# Iterate through invalid kinds and add them to file if they do not clash with other CRDs
+print(f"\nFollowing kinds are added to the '{SKIP_KINDS_FILE}' file:")
+with open(skip_kinds_file, "w+") as skf:
+    for i in sorted(skip_kinds):
+        print(i)
+    skf.write(",".join(sorted(skip_kinds)))
+
+if len(delete_list) > 0:
+    print("\nThe following API resources do not have valid OpenAPI specifications:")
+
+for del_item in sorted(delete_list):
+    print(del_item)
+    del definitions[del_item]
+
 with open(spec_file, "w") as openapi_spec_file:
-    for type_name in definitions:
-        type_def = definitions[type_name]
-        if "x-kubernetes-group-version-kind" in type_def:
-            for kube_ext in type_def["x-kubernetes-group-version-kind"]:
-                if "properties" not in type_def:
-                    delete_list.append(type_name)
-
-    if len(delete_list) > 0:
-        print("The following API resources do not have valid OpenAPI specifications:\n")
-
-    for del_item in delete_list:
-        print(del_item)
-        del definitions[del_item]
     openapi_spec_file.write(json.dumps(openapi_data, indent=2))
 
 if args.dryrun:
@@ -123,7 +178,7 @@ if args.dryrun:
 destination = args.destination
 
 if destination is None:
-    destination = "{}/openshift-json-schema".format(script_dir)
+    destination = "{}/schemas".format(script_dir)
 
 output_destination = os.path.join(
     destination, "{}-standalone".format(args.openshift_version))
